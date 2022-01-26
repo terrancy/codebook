2e126485336c6c8a259152f5b1e52deb25968293
terrancy <terrancy@qq.com>
2022-01-26 23:19:11 +0800

    feat:图论

--- graph/islands.go
+++ graph/islands.go
@@ -1,4 +1,4 @@
-package others
+package graph
 
 // 岛屿问题
 
--- test/graph_test.go
+++ test/graph_test.go
@@ -1,1 +1,24 @@
 package test
+
+import (
+    "awesome/graph"
+    "fmt"
+    "testing"
+)
+
+//
+//  TestNumIslands
+//  @Description: NC109 岛屿数量
+//  @param t
+//
+func TestNumIslands(t *testing.T) {
+    grid := [][]byte{
+        {'1', '1', '0', '0', '0'},
+        {'0', '1', '0', '1', '1'},
+        {'0', '0', '0', '1', '1'},
+        {'0', '0', '0', '0', '0'},
+        {'0', '0', '1', '1', '1'},
+    }
+    cnt := graph.NumIslands(grid)
+    fmt.Println(cnt)
+}
