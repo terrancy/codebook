b654bd9c89f26c6444072457a2e98fa544b805d3
terrancy <terrancy@qq.com>
2022-06-04 17:52:26 +0800

    feat:每日一题:929. 独特的电子邮件地址

---
+++ challenge/NumUniqueEmails.go
@@ -0,0 +1,27 @@
+package challenge
+
+import "strings"
+
+//
+//  NumUniqueEmail
+//  @Description: 929. 独特的电子邮件地址
+//  @param emails
+//  @return int
+//
+func NumUniqueEmail(emails []string) int {
+    dict := map[string]struct{}{}
+    temp := ""
+    idx := 0
+    for _, email := range emails {
+        // 获取@的前部分
+        idx = strings.Index(email, "@")
+        // 保留第一个+之前的字符串
+        temp = strings.SplitN(email[:idx], "+", 2)[0]
+        // 过滤.
+        temp = strings.ReplaceAll(temp, ".", "")
+        // 去重
+        dict[temp+email[idx:]] = struct{}{}
+    }
+    // 计算
+    return len(dict)
+}
--- test/challenge_june_test.go
+++ test/challenge_june_test.go
@@ -15,3 +15,18 @@ func TestConsecutiveNumbersSum(t *testing.T) {
     res := challenge.ConsecutiveNumbersSum(5)
     fmt.Println(res)
 }
+
+//
+//  TestNumUniqueEmails
+//  @Description: 929. 独特的电子邮件地址
+//  @param t
+//
+func TestNumUniqueEmails(t *testing.T) {
+    emails := []string{
+        "test.email+alex@leetcode.com",
+        "test.e.mail+bob.cathy@leetcode.com",
+        "testemail+david@lee.tcode.com",
+    }
+    res := challenge.NumUniqueEmail(emails)
+    fmt.Println(res)
+}
